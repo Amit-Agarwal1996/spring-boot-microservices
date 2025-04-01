37b51b114323024ec993a5c46cc20531d3d0fbff
Amit-Agarwal1996 <40624067+Amit-Agarwal1996@users.noreply.github.com>
2025-04-01 04:27:54 +0000

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ counterservice/src/test/java/com/marcuseisele/springboot_microservices/counterservice/api/Categories.java
@@ -0,0 +1,12 @@
+package com.marcuseisele.springboot_microservices.counterservice.api;
+ public interface Categories{
+    
+      interface valid{}
+      interface invalid{}
+      interface boundary{}
+      interface integration{}
+    
+    interface getCount{}
+    
+   
+}
